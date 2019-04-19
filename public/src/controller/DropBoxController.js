efb23afbde1eff61da8c99ba3cff553b9ce2563e
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-04-19 08:43:58 -0300

    Adding support to use Promise.all()

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -22,11 +22,62 @@ class DropBoxController {
         this.inputFilesEl.addEventListener('change', event => {
 
             //Get the files from the "target" (that means, the inputFilesEl)
-            console.log(event.target.files);
+            this.uploadTask(event.target.files);
 
             this.snackModalEl.style.display = "block";
 
         });
+
+    }
+
+    uploadTask(files){
+
+        //Create promises to handle the uploaded files
+        let promises = [];
+
+        //Converts the files collection into array
+        [...files].forEach( file => {
+
+            //Pushes the promise to the promises array
+            promises.push(new Promise((resolve, reject) => {
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+
+                    try {
+
+                        resolve(JSON.parse(ajax.responseText));
+
+                    } catch (e){
+
+                        reject(e);
+
+                    }
+
+                };
+
+                ajax.onerror = event => {
+
+                    reject(event);
+
+                }
+
+                //Lets use the formDataApi
+                let formData = new FormData();
+
+                //Sends the file inside the "input-file" named parameter to the server
+                formData.append('input-file', file);
+
+                ajax.send(formData);
+
+            }));
+
+        });
+
+        return Promise.all(promises);
 
     }
 
