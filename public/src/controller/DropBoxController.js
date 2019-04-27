0e41658f6bfe49e8b5ce223b924fd62d48f4b104
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-04-27 15:17:54 -0300

    Adding click events on dropbox files

--- public/src/controller/DropBoxController.js
+++ public/src/controller/DropBoxController.js
@@ -394,6 +394,8 @@ class DropBoxController {
             <div class="name text-center">${file.name}</div>
         `;
 
+        this.initEventsLi(li);
+
         return li;
 
     }
@@ -415,6 +417,62 @@ class DropBoxController {
                 this.listFilesEl.appendChild(this.getFileView(data, key));
 
             });
+
+        });
+
+    }
+
+    initEventsLi(li){
+
+        li.addEventListener('click', e=> {
+
+            if (e.shiftKey) {
+
+                let firstLi = this.listFilesEl.querySelector('.selected');
+
+                if (firstLi) {
+
+                    let indexStart;
+                    let indexEnd;
+                    let lis = li.parentElement.childNodes;
+
+                    lis.forEach((el, index) => {
+
+                        if (firstLi === el) indexStart = index;
+
+                        if (li === el) indexEnd = index;
+
+                    });
+
+                    let index = [indexStart, indexEnd].sort();
+
+                    lis.forEach((el, i) => {
+
+                        if (i >= index[0] && i <= index[1]){
+
+                            el.classList.add('selected');
+
+                        }
+
+                    });
+
+                    return true;
+                    
+                }
+
+            }
+
+            if (!e.ctrlKey) {
+
+                this.listFilesEl.querySelectorAll('li.selected').forEach( el => {
+
+                    el.classList.remove('selected');
+
+                });
+
+            }
+
+            li.classList.toggle('selected');
 
         });
 
